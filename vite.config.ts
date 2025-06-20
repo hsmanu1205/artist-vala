0d82726c018b7fbb7c316d7175a221a1e7aa27a1
Harshit Singh <harshit.singh.hs9454@gmail.com>
2025-06-20 23:16:17 +0530

    added package

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  define: {
-    'process.env': process.env
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      target: 'es2020',
-      supported: {
-        'top-level-await': true
-      }
-    }
-  },
-  build: {
-    target: 'es2020',
-    rollupOptions: {
-      external: ['react', 'react-dom']
-    }
-  }
-});
+  plugins: [react()],
+})
